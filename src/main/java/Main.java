71e438598f2f974cc67412661e44cb146007151f
Roman Bulinski <aboandrus@gmail.com>
2019-07-07 17:05:15 +0200

    Implemented code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,15 @@
 public class Main {
 
+
+    public static int bouncingBall(double h, double bounce, double window) {
+
+        int counter =0;
+        while(h > window){
+            h=h*bounce;
+            counter+=2;
+        }
+        return counter-1;
+    }
     
 
 }
--- src/test/java/TasMain.java
+++ src/test/java/TasMain.java
@@ -1,7 +1,16 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TasMain {
 
     @Test
+    public void test1() {
+        assertEquals(3, Main.bouncingBall(3.0, 0.66, 1.5));
+    }
+    @Test
+    public void test2() {
+        assertEquals(15, Main.bouncingBall(30.0, 0.66, 1.5));
+    }
 
 }
